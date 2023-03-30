ec4b36ca1dd5fb30671ad2b709ba0f2880496157
jay98patel <itsj10.jey@gmail.com>
2023-03-30 21:55:18 +0530

    removed unwanted code

--- src/App.vue
+++ src/App.vue
@@ -5,25 +5,12 @@
 </template>
 
 <script>
-// import ThankYouComponent from "@/components/ThankYouComponent.vue";
-// import WelcomeComponent from "@/components/WelcomeComponent.vue";
-
 export default {
   name: "App",
-  components: {
-    // ThankYouComponent,
-    // WelcomeComponent,
-  },
 };
 </script>
 
 <style lang="scss">
-html,
-body {
-  height: 100%;
-  overflow: hidden;
-}
-
 #app {
   color: black;
 }
